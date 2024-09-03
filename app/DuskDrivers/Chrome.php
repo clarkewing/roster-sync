bb84b51dd7b60a6101550d72f5c4cc59ef1070ca
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2024-09-03 22:31:22 +0200

    Add desktop user agent

--- app/DuskDrivers/Chrome.php
+++ app/DuskDrivers/Chrome.php
@@ -12,20 +12,25 @@ class Chrome extends ConsoleDuskChrome
     // Overwrite Driver creation to log ALL console message levels.
     public function getDriver()
     {
-        $options = (new ChromeOptions())->addArguments(
-            array_filter(array_merge(
-                config('laravel-console-dusk.driver.chrome.options', []),
-                [$this->runHeadless()]
-            ))
-        );
+        $options = (new ChromeOptions())
+            ->addArguments(
+                array_filter(array_merge(
+                    config('laravel-console-dusk.driver.chrome.options', []),
+                    [$this->runHeadless()]
+                ))
+            )
+            ->setExperimentalOption('mobileEmulation', [
+                'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+            ]);
 
         $driver = RemoteWebDriver::create(
             $_ENV['DUSK_DRIVER_URL'] ?? 'http://localhost:9515',
             DesiredCapabilities::chrome()
                 ->setCapability(
                     ChromeOptions::CAPABILITY,
-                    $options
-                )->setCapability('goog:loggingPrefs', ['browser'=>'ALL'])
+                    $options,
+                )
+                ->setCapability('goog:loggingPrefs', ['browser' => 'ALL'])
         );
 
         $driver->executeCustomCommand(
